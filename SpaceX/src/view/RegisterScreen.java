f6a0eb6bdf0532aa4dd960867c13c0932774d44d
julianaacruz <juliana201380cc@gmail.com>
2020-11-16 16:54:35 -0500

    Labels del log in y parte de documentación

--- SpaceX/src/view/RegisterScreen.java
+++ SpaceX/src/view/RegisterScreen.java
@@ -2,9 +2,17 @@ package view;
 
 import processing.core.PApplet;
 import processing.core.PFont;
-import processing.core.PImage;
+import processing.core.PImage; 
 
-public class RegisterScreen { //Register Screen
+/**
+ * Register Screen class 
+
+ * @author: Juliana Cruz, Nicolás Montero
+
+ * @version: 11/16/2020
+
+ */
+public class RegisterScreen { 
 	private PApplet app;
 	private boolean name=true, lastname=true, email=true, password=true, nationality=true, cellphone=true;
 	
@@ -23,37 +31,37 @@ public class RegisterScreen { //Register Screen
 		// Labels -- must disappear to write on textfields
 		if (name==true) {
 			app.text("Name", 30, 200);
-			if(app.mousePressed == true && app.mouseX > 30 && app.mouseX < 300 && app.mouseY > 185 && app.mouseY < 215) {
+			if(app.mousePressed == true && app.mouseX > 30 && app.mouseX < 300 && app.mouseY > 180 && app.mouseY < 215) {
 				name=false;
 			}
 		}
 		if (lastname==true) {
 			app.text("Last name", 30, 260);
-			if(app.mousePressed == true && app.mouseX > 30 && app.mouseX < 300 && app.mouseY > 245 && app.mouseY < 275) {
+			if(app.mousePressed == true && app.mouseX > 30 && app.mouseX < 300 && app.mouseY > 240 && app.mouseY < 275) {
 				lastname=false;
 			}
 		}
 		if (email==true) {
 			app.text("Email", 30, 320);
-			if(app.mousePressed == true && app.mouseX > 30 && app.mouseX < 300 && app.mouseY > 305 && app.mouseY < 335) {
+			if(app.mousePressed == true && app.mouseX > 30 && app.mouseX < 300 && app.mouseY > 300 && app.mouseY < 335) {
 				email=false;
 			}
 		}
 		if (password==true) {
 			app.text("Password", 30, 380);
-			if(app.mousePressed == true && app.mouseX > 30 && app.mouseX < 300 && app.mouseY > 365 && app.mouseY < 395) {
+			if(app.mousePressed == true && app.mouseX > 30 && app.mouseX < 300 && app.mouseY > 360 && app.mouseY < 395) {
 				password=false;
 			}
 		}
 		if (nationality==true) {
 			app.text("Nationality", 30, 440);
-			if(app.mousePressed == true && app.mouseX > 30 && app.mouseX < 300 && app.mouseY > 425 && app.mouseY < 455) {
+			if(app.mousePressed == true && app.mouseX > 30 && app.mouseX < 300 && app.mouseY > 420 && app.mouseY < 455) {
 				nationality=false;
 			}
 		}
 		if (cellphone==true) {
 			app.text("Cellphone", 30, 500);
-			if(app.mousePressed == true && app.mouseX > 30 && app.mouseX < 300 && app.mouseY > 485 && app.mouseY < 515) {
+			if(app.mousePressed == true && app.mouseX > 30 && app.mouseX < 300 && app.mouseY > 480 && app.mouseY < 515) {
 				cellphone=false;
 			}
 		}
